190e7ea167b5b05092490f31bf0531b61a87ab09
Sergei Egorov <bsideup@gmail.com>
2017-03-23 18:57:14 +0200

    Update projects.kts

--- projects.kts
+++ projects.kts
@@ -10,7 +10,7 @@ project("KamaHQ/dummy") {
     }
 }
 
-project("KamaHQ/dummy1") {
+project("KamaHQ/dummy-fork") {
     repository = GitHubRepository("KamaHQ", "testcontainers-java-examples")
 
     triggers {
